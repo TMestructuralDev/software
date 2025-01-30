cb130f8c4c11876974daea618d89525a9f373b7a
ventas <ventas@example.com>
2025-01-30 16:55:22 -0700

    Agregado metodo str a ingenieros

--- src/tmsoftware/autenticacion/models.py
+++ src/tmsoftware/autenticacion/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import User
 class Ingeniero(models.Model):
     ingeniero = models.OneToOneField(User, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombre 
