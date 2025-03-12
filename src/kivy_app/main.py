0de6b0d3fb1d99c008ebf4a70528e3c53541b0a9
ventas <ventas@example.com>
2025-03-12 12:45:25 -0700

    Modificado main agregado direcciones os Modificado git ignore

--- src/kivy_app/main.py
+++ src/kivy_app/main.py
@@ -1,16 +1,21 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from controllers.nav_controller import NavigationController
+import os
 
 class MyApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark" 
         #self.theme_cls.primary_palette = "Orange"  # Si no quieres usar naranja, cámbialo a otro color
         
-        Builder.load_file("kv/home.kv")  # Se carga el archivo KV 
-        Builder.load_file("kv/notas_gruas.kv")
-        Builder.load_file("kv/estructural.kv")
-        Builder.load_file("kv/login.kv")
+        # Asegúrate de que los archivos .kv se carguen correctamente incluso cuando se empaqueten.
+        kv_dir = os.path.join(os.path.dirname(__file__), 'kv')
+        
+        # Cargar los archivos .kv
+        Builder.load_file(os.path.join(kv_dir, 'home.kv'))
+        Builder.load_file(os.path.join(kv_dir, 'notas_gruas.kv'))
+        Builder.load_file(os.path.join(kv_dir, 'estructural.kv'))
+        Builder.load_file(os.path.join(kv_dir, 'login.kv'))
         
         return NavigationController()
 
