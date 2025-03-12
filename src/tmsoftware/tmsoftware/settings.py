78193c6612ef7e9e81d29653b204419bb7d0fa22
ventas <ventas@example.com>
2025-03-12 09:28:34 -0700

    Modificado settings del backend

--- src/tmsoftware/tmsoftware/settings.py
+++ src/tmsoftware/tmsoftware/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-59mv^24gax*o^^hnw^7#n2ifdtsg3t0*2obeq2a2eq8uv%ap&d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '10.0.2.2', '192.168.100.7']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.100.7', '10.0.2.2']
 
 
 # Application definition
